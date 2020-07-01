f439f02a90566d113496cc7a65f182618590643c
Danilo Tuler <danilo@cartesi.io>
2020-06-30 23:14:34 -0400

    Bump playground

--- helloworld/cartesi-machine/build-cartesi-machine.sh
+++ helloworld/cartesi-machine/build-cartesi-machine.sh
@@ -1,7 +1,7 @@
 # general definitions
 MACHINES_DIR=.
 MACHINE_TEMP_DIR=__temp_machine
-CARTESI_PLAYGROUND_DOCKER=cartesi/playground:0.1.0
+CARTESI_PLAYGROUND_DOCKER=cartesi/playground:0.1.1
 
 # set machines directory to specified path if provided
 if [ $1 ]; then
