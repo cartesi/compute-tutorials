163adf5a7253b2821802fffb98bc26c96ce1bb74
Carsten Munk <carsten@zippie.com>
2023-04-26 16:29:29 +0200

    Make build-libscrypt-riscv64.sh work sanely with playground 0.5.0
    Signed-off-by: Carsten Munk <carsten@zippie.com>

--- dogecoin-hash/cartesi-machine/build-libscrypt-riscv64.sh
+++ dogecoin-hash/cartesi-machine/build-libscrypt-riscv64.sh
@@ -7,17 +7,27 @@ CARTESI_PLAYGROUND_DOCKER=cartesi/playground:0.5.0
 # - sets "PATH" to include the home directory (working directory), so as to call "riscv64-cartesi-linux-gnu-ar" when "ar" is invoked by the Makefile
 # - calls "make clean" and "make" to build libscrypt
 docker run \
+   -v `pwd`:/home/$(id -u -n) \
+   --name libscrypt-build \
+   $CARTESI_PLAYGROUND_DOCKER \
+   /bin/bash -c 'apt-get update && apt-get install -y make'
+
+docker start libscrypt-build
+
+docker exec \
   -e USER=$(id -u -n) \
   -e GROUP=$(id -g -n) \
   -e UID=$(id -u) \
   -e GID=$(id -g) \
-  -v `pwd`:/home/$(id -u -n) \
   -w /home/$(id -u -n) \
-  --rm $CARTESI_PLAYGROUND_DOCKER /bin/bash -c '\
+  libscrypt-build /usr/local/bin/entrypoint.sh /bin/bash -c '\
     cd libscrypt ;\
     export CC=riscv64-cartesi-linux-gnu-gcc ;\
     export CFLAGS_EXTRA="-Wl,-rpath=. -O2 -Wall -g" ;\
-    export PATH=$PATH:~/ ;\
+    export PATH=$PATH:/home/$(id -u -n) ;\
     make clean ;\
     make ;\
   '
+
+docker stop libscrypt-build
+docker rm libscrypt-build
